55e7a7bbe9410a110f446b7c0e5ae8b810a93144
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2025-03-02 22:09:51 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 
 import streamlit as st
 
-st.markdown("### Chat with NDP")
-st.markdown("""Pose a question related to free large language models hosted on Hugging 
-               Face. Search the NDP data catalog to find the desired datasets.""")
+st.markdown("### Chat with The Task Force Data Hub Catalog")
+st.markdown("""Pose a question related to California Landscape Metrics. Search The Task Force Data Hub Catalog to find the desired datasets.""")
