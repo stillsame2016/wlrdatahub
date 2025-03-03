48dc7999c5b2d3081654bfb8c053c33ebc96baee
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2025-03-02 22:13:33 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,13 @@
 import streamlit as st
 
 st.markdown("### Chat with The Task Force Data Hub Catalog")
-st.markdown("""Pose a question related to California Landscape Metrics. 
-               Search The Task Force Data Hub Catalog to find the desired datasets.
-            """)
 
 # Session state
 if "messages" not in st.session_state:
-    st.session_state.messages = [{
-        "role": "assistant",
-        "content": "Ask me about available raster datasets! I can help you find and visualize spatial data."
-    }]
+    st.session_state.messages = []
+
+# Chat interface
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+
