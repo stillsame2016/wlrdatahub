a765626a2f656bc9f1fe98067196081bd299be3c
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2025-03-02 22:23:46 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 
+import json
+import requests
 import streamlit as st
 
 st.markdown("### Chat with The Task Force Data Hub Catalog")
@@ -15,8 +17,22 @@ if prompt := st.chat_input("What can I help you with?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
 
+    response = requests.get(f"https://sparcal.sdsc.edu/api/v1/Utility/clm?search_terms={prompt}")
+    datasets = json.loads(response.text)
+    st.code(json.dumps(datasets, indent=4))
+    
     with st.spinner("Thinking ..."):
         st.session_state.messages.append({"role": "assistant", "content": prompt})
         st.chat_message("assistant").write(prompt)
+
+
+
+
+
+
+
+
+
+
 
 
