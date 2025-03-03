adf78ef8582c233474bb493a522bc5f2c7da4d09
stillsame2016 <165528428+stillsame2016@users.noreply.github.com>
2025-03-02 22:11:48 -0800

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,13 @@
 import streamlit as st
 
 st.markdown("### Chat with The Task Force Data Hub Catalog")
-st.markdown("""Pose a question related to California Landscape Metrics. Search The Task Force Data Hub Catalog to find the desired datasets.""")
+st.markdown("""Pose a question related to California Landscape Metrics. 
+               Search The Task Force Data Hub Catalog to find the desired datasets.
+            """)
+
+# Session state
+if "messages" not in st.session_state:
+    st.session_state.messages = [{
+        "role": "assistant",
+        "content": "Ask me about available raster datasets! I can help you find and visualize spatial data."
+    }]
